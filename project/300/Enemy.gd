40c16efcba70823535db2f2d33db4bae3bd898d9
dt <dhtxhg@gmail.com>
2022-04-03 11:48:24 -0700

    enemy death and tweaks

--- project/300/Enemy.gd
+++ project/300/Enemy.gd
@@ -1,6 +1,9 @@
 extends PathFollow2D
 
-export(float) var walk_speed = 0.03
+export(float) var walk_speed = 0.05
+export(float) var death_duration = 1.0
+onready var timer = $DeathTimer
+onready var alpha_mod = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +13,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	unit_offset += walk_speed * delta
+	
+	# alpha fade in/out on spawn/death
+	if not timer.is_stopped():
+		alpha_mod -= delta/1.5
+		$EnemySprite.modulate = Color(1.0,1.0,1.0,alpha_mod)
+	elif alpha_mod < 1.0:
+		alpha_mod += delta/6.0
+		$EnemySprite.modulate = Color(1.0,1.0,1.0,alpha_mod)
 	pass
 
 func wiggle(max_x=3, max_y=3):
@@ -17,5 +28,11 @@ func wiggle(max_x=3, max_y=3):
 	v_offset += (randf()-0.5) * 1
 
 func die():
-	$EnemySprite.play('death')
+	# on play sound/anim on first death hit
+	if timer.is_stopped():
+		$EnemySprite.play('death')
+		$DeathSound.play()
+		walk_speed = 0
+	timer.start(death_duration)
+	yield(timer, "timeout")
 	queue_free()
