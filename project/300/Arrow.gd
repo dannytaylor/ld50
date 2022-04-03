27782c4fa1b1dc1b9d1be71c3252939a8ba219b1
Devin Pentecost <devinpentecost@gmail.com>
2022-04-03 00:32:12 -0700

    Wacky arrow madness

---
+++ project/300/Arrow.gd
@@ -0,0 +1,13 @@
+extends PathFollow2D
+
+
+export(float) var speed = 50
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	offset += speed * delta
+	if unit_offset >= 1:
+		die()
+	
+func die():
+	get_parent().queue_free()
